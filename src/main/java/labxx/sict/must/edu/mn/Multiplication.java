bb3975a71962436255f34aca7ed4d83f9a564801
otgoo1206 <133503593+otgoo1206@users.noreply.github.com>
2025-05-09 10:17:01 +0800

    Merge conflict шийдсэн

--- src/main/java/labxx/sict/must/edu/mn/Multiplication.java
+++ src/main/java/labxx/sict/must/edu/mn/Multiplication.java
@@ -1,15 +1,8 @@
 package labxx.sict.must.edu.mn;
 
 public class Multiplication {
-   /**
- * Multiplies two integers and returns the product.
- * @param a first integer
- * @param b second integer
- * @return product of a and b
- */
-public int multiply(int a, int b) {
-    return a * b;
-}
-
+    public double multiply(double a, double b) {
+        return a * b;
+    }
 }
 
